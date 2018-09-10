db8316e07acd7275844383bad8925264fd49935b
Aluno <Aluno@GRVL301B-03>
2018-09-10 20:50:58 -0300

    layout mobile

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,6 +10,8 @@ and open the template in the editor.
         <title>Meu Projeto Framework</title>
         
               <!--Bootstrap-->
+             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+             
               <link href="<?php echo base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">  
               <link href="<?php echo base_url('assets/css/bootstrap-grid.min.css') ?>" rel="stylesheet">  
                     
